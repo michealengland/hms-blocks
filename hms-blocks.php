45f6cb6073734482c67a64551ff1b45d4f0794ed
Mike England <mengland@thinkholmes.com>
2018-09-13 15:53:03 -0400

    Updated version on plugin

--- hms-blocks.php
+++ hms-blocks.php
@@ -7,7 +7,7 @@
  * Domain Path: /languages
  * Author: Mike England @mikelikethebike
  * Author URI: https://twitter.com/mikelikethebike
- * Version: 1.0.0
+ * Version: 1.0.1
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
